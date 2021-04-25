eae13dd427dd4667700f7a9306931743e426eb19
Alex Evgrashin <aevgrashin@yandex.ru>
2021-04-25 12:32:06 +0300

    Added graphics

---
+++ Content.Shared/GameObjects/Components/Janitorial/SharedJanitorialCartVisuals.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Shared.GameObjects.Components.Janitorial
+{
+    public enum JanitorialCartLayers : byte
+    {
+        Base,
+        TrashBag,
+        Mop,
+        LightReplacer,
+        Signs,
+        Spray,
+        Liquid
+    }
+}
